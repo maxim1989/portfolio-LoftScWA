0112c9b26495b41f1d88b905d94af39840ef6ae2
Fedorov Max <fedorov-max89@mail.ru>
2017-07-05 17:40:34 +0300

    #11.home_work_3 Work on bugs

--- source/js/flip.js
+++ source/js/flip.js
@@ -3,23 +3,19 @@ $(document).ready(function () {
         btn = $(".header-hello__auth"),
         wrapper = $(".wrapper-hello"),
         flag = false;
-    clickListener(btn, cards, wrapper);
-    function clickListener(btn, cards, wrapper) {
-        btn.on( "click", function(e) {
-            console.log(1, e);
-            if (e.target.className == "header-hello__auth" || e.target.className == "header-hello__auth-btn") {
-                cards.toggleClass('flipped');
-                btn.css({display: 'none'});
-                flag = true;
-            }
-        });
-        wrapper.on("click", function (e) {
-            console.log(2, e);
-            if (e.target.className == "wrapper wrapper-hello" && flag) {
-                cards.toggleClass('flipped');
-                btn.css({display: 'block'});
-                flag = false;
-            }
-        })
-    }
+
+    btn.on( "click", function(e) {
+        if (e.target.className == "header-hello__auth" || e.target.className == "header-hello__auth-btn") {
+            cards.toggleClass('flipped');
+            btn.css({visibility: 'hidden'});
+            flag = true;
+        }
+    });
+    wrapper.on("click", function (e) {
+        if (e.target.className == "wrapper wrapper-hello" && flag) {
+            cards.toggleClass('flipped');
+            btn.css({visibility: 'visible'});
+            flag = false;
+        }
+    })
 });
--- source/js/menu.js
+++ source/js/menu.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
     let menu = document.querySelector(".burger.burger_type_menu"),
         close = document.querySelector(".modal__close-link"),
-        setStyle = function (styleOne, styleTwo) {
+        body = document.getElementsByTagName("body"),
+        setStyle = function (styleOne, styleTwo, hideScroll) {
             let modal = document.querySelector(".modal");
             menu.style.display = styleOne;
             modal.style.display = styleTwo;
+            body[0].style.overflow = hideScroll;
         };
 
     if (menu) {
         menu.addEventListener("click", function (e) {
-            setStyle("none", "block");
+            setStyle("none", "block", "hidden");
         });
         close.addEventListener("click", function (e) {
-            setStyle("block", "none");
+            setStyle("block", "none", "auto");
         })
     }
 });
