ff2618a3d449d04c50d45bea1e143b6955857f9a
Fedorov Max <fedorov-max89@mail.ru>
2017-07-08 19:27:04 +0300

    #13.home_work_4 Sidebar and easy preloader

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -5,5 +5,7 @@ module.exports = [
   './source/js/mouseParallax.js',
   './source/js/flip.js',
   './source/js/blur.js',
-  './source/js/menu.js'
+  './source/js/menu.js',
+  './source/js/sidebar.js',
+  './source/js/preloader.js'
 ];
